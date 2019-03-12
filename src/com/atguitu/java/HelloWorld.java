e7f026760ac4b04d42cc321444b309cf4ccc8b40
xiaosongbing <wzz.157671321>
2019-03-12 18:55:25 +0800

    1111

---
+++ src/com/atguitu/java/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.atguitu.java;
+
+/**
+ * @author wzz
+ * @date 2019/3/12 - 17:57
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("test。。。。");
+        System.out.println("test。。。。");
+        System.out.println("test。。。。");
+
+    }
+
+}
